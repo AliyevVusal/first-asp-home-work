1f5a563987e365778749806ec551d83daa159d5e
AliyevVusal <vusalma@code.edu.az>
2022-06-01 08:41:59 +0400

    BAckAndFront

---
+++ MyFirstHomeWork(WebApplication)/Controlers/TeamController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstHomeWork_WebApplication_.Controlers
+{
+    public class TeamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
